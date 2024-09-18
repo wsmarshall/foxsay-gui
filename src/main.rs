4c67d8d2f8fc0503cef45d80b90af1c8766e7209
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-18 13:08:51 -0700

    adds images and image to program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Box as GtkBox, Image, Label, Orientation};
 
 fn main() {
     let app = Application::new(Some("io.github.wsmarshall.foxsay-gui"), Default::default());
@@ -7,7 +7,17 @@ fn main() {
     app.connect_activate(|app| {
         let window = ApplicationWindow::new(app);
         window.set_title("Foxsay");
-        window.set_default_size(451, 82);
+        window.set_default_size(1300, 1000);
+
+        let layout_box = GtkBox::new(Orientation::Vertical, 0);
+
+        let label = Label::new(Some("Wa-pa-pa-pa-pa-pa-pow!\n   \\\n    \\"));
+        layout_box.add(&label);
+
+        let fox_image = Image::from_file("./images/fox.png");
+        layout_box.add(&fox_image);
+
+        window.add(&layout_box);
 
         window.show_all();
     });
