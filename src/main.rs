40cb34f61005e67598040a6a6062ab5028eeddda
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2024-09-20 13:25:14 -0700

    makes a quick variable rename

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn build_ui(app: &gtk::Application) {
     let glade_src = include_str!("layout.glade");
     let builder = gtk::Builder::from_string(&glade_src);
 
-    let window: gtk::Window = builder.object("application window").unwrap();
+    let window: gtk::Window = builder.object("application_window").unwrap();
     window.set_application(Some(app));
 
     window.show_all();
@@ -16,5 +16,5 @@ fn main() {
 
     application.connect_activate(build_ui);
     application.run();
-    
+
 }
